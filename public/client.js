e900172fc9fc5e5baeade50ac348d98469ad4100
xSefa <50076109+xSefa@users.noreply.github.com>
2020-01-26 18:09:13 +0300

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
-console.log("hello world :O");
+console.log("hello world :o");
 
 // our default array of dreams
 const dreams = [
